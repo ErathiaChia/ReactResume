7ce98eaefec625249c1f82072d91e5ecfc367433
Erathia Xie <Ecytchia@gmail.com>
2023-07-27 16:30:14 +0800

    not very sure what's the changes

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,31 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
+      <head>
+        <title>E.E Xie - Resume</title>
+        {/* favicon */}
+        <link rel="icon" href="assets/Logo_V2.1.svg" />
 
+        {/* <!-- fonts --> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=Roboto&family=Saira+Stencil+One&display=swap"
+        />
+
+        {/* <!-- css libraries file --> */}
+        <link rel="stylesheet" href="styles/libraries.min.css" />
+
+        {/* <!-- bootstrap css file --> */}
+        <link rel="stylesheet" href="styles/bootstrap.css" />
+        {/* <!-- main style file --> */}
+        <link rel="stylesheet" href="styles/main.css" />
+      </head>
+
+      <body>
+
+      </body>
     </main>
   )
 }
